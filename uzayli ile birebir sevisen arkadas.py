e4afa29350e7479bb9adf17c4c55a5a0ceee66db
Ege <egeucak75@hotmail.com>
2017-02-19 21:55:50 +0300

    first commit

---
+++ uzayli ile birebir sevisen arkadas.py
@@ -0,0 +1,11 @@
+import random
+garavel = open("adim garavel.txt", "r").read().splitlines()
+for i in range(10):
+	x = 0
+	while (x == 0):
+		secilen = random.choice(garavel).strip()
+		if (len(secilen) > 0):
+			x = 1
+	if "=" in secilen:
+		secilen = secilen.split("=")[1]	
+	print(secilen)
